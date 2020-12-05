b3a58c6d0fa3ce88b7884b45f817743b30156dff
MarlonMiguel <marlonmiguel1993@gmail.com>
2020-12-04 23:35:30 -0300

    Configurações no eslint e instalação do prettier

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # barber-api
 Application for barbers studios all platforms
+
+-Run app with command
+-yarn dev:server
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
+routes.use('/appointments', appointmentsRouter);
 
 routes.get('/', (req, resp) => resp.json({ message: 'hello world!!' }));
 
