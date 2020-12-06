2aa41a23f60010672a1091811dad3a3b61e8cd8d
MarlonMiguel <marlonmiguel1993@gmail.com>
2020-12-06 10:10:18 -0300

    Criado get de todos os itens e aplicação do conceito de DTO

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
+
 routes.use('/appointments', appointmentsRouter);
 
 routes.get('/', (req, resp) => resp.json({ message: 'hello world!!' }));
