49e7290a4f6895d2b29c47178e8237afa4df9466
MarlonMiguel <marlonmiguel1993@gmail.com>
2020-12-07 01:16:55 -0300

    Criada primeira migration para appointments

---
+++ src/databases/migrations/1607312614223-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1607312614223 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
